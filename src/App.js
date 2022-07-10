3b73ead2c73c3caebf304651dd53510ab7cea6f9
jyaymie <jamiesujiepark@gmail.com>
2022-07-09 23:48:06 -0700

    Dynamically create a form for making quiz questions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React, { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { DataContext } from './dataContext';
+import NewQuizForm from './components/NewQuizForm/NewQuizForm';
+import NewQuizQuestions from './components/NewQuizQuestions/NewQuizQuestions';
 
 function App() {
-  return (
-    <div>
-      <h1>Go BadJJR!</h1>
-    </div>
-  );
+	const [numOfQuestions, setNumOfQuestions] = useState('');
+
+	return (
+		<DataContext.Provider value={{ numOfQuestions, setNumOfQuestions }}>
+			<div>
+				<header>
+					<h1>
+						<Link to='/'>🦡</Link>
+					</h1>
+				</header>
+				<main>
+					<Routes>
+						<Route path='/new-quiz-form' element={<NewQuizForm />} />
+						<Route
+							path='/new-quiz-questions'
+							element={<NewQuizQuestions />}></Route>
+					</Routes>
+				</main>
+			</div>
+		</DataContext.Provider>
+	);
 }
 
 export default App;
