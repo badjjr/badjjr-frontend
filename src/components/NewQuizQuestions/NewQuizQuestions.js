f90407edce88a92b68058d22138eec32652e4229
jyaymie <jamiesujiepark@gmail.com>
2022-07-10 22:55:56 -0700

    Use Context for quiz data / Dynamically add input fields for additional incorrect answers

--- src/components/NewQuizQuestions/NewQuizQuestions.js
+++ src/components/NewQuizQuestions/NewQuizQuestions.js
@@ -1,26 +1,44 @@
 import './NewQuizQuestions.css';
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { DataContext } from '../../dataContext';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 function NewQuizQuestions() {
-	const { numOfQuestions } = useContext(DataContext);
-	const [questions, setQuestions] = useState([]);
+	const { quizQuestions, setQuizQuestions } = useContext(DataContext);
 
-	useEffect(() => {
-		let questionsArray = new Array(parseInt(numOfQuestions)).fill({});
-		setQuestions(questionsArray);
-	}, [numOfQuestions]);
+	// By default, there is an input field for just one incorrect answer.
+	// Allow the user to add more incorrect answers to a question.
+	function handleIncorrectAnswerAdd(questionIndex) {
+		const updatedQuestions = quizQuestions.map((question, index) => {
+			// If the user added more incorrect answers to a question, identify that
+			// specific question, indicated by its questionIndex, in the list of
+			// questions.
+			if (index === questionIndex) {
+				// Add the user's new entries to that question's incorrectAnswer array.
+				const incorrectAnswer = [...question.incorrectAnswer, ''];
+				// Update the question's data.
+				return {
+					...question,
+					incorrectAnswer,
+				};
+			}
+			return question;
+		});
+		// Sync changes to quiz questions to React context so that all components
+		// can access the updated information.
+		setQuizQuestions(updatedQuestions);
+	}
 
 	return (
 		<div>
+			{/* Render the form with Bootstrap styling. */}
 			<Form className='questions-form'>
-				{questions.map((question, index) => (
-					<div key={index}>
-						<h3 className='question-title'>Question {index + 1}</h3>
+				{quizQuestions.map((question, questionIndex) => (
+					<div key={questionIndex}>
+						<h2 className='question-title'>Question {questionIndex + 1}</h2>
 						<Form.Group>
-							<Form.Label htmlFor='type'>Question Type</Form.Label>
+							<Form.Label>Question Type</Form.Label>
 							<Form.Select>
 								<option value='1'>Multiple Choice</option>
 								<option value='2'>True / False</option>
@@ -28,18 +46,40 @@ function NewQuizQuestions() {
 						</Form.Group>
 						<Form.Group>
 							<Form.Label htmlFor='question'>Question</Form.Label>
-							<Form.Control type='text' id='question' required />
+							<Form.Control
+								type='text'
+								id='question'
+								placeholder='Enter a question or True/False statement'
+								required
+							/>
 						</Form.Group>
 						<Form.Group>
 							<Form.Label htmlFor='correct-answer'>Correct Answer</Form.Label>
-							<Form.Control type='text' id='correct-answer' required />
+							<Form.Control
+								type='text'
+								id='correct-answer'
+								placeholder='Enter the correct answer'
+								required
+							/>
 						</Form.Group>
-						<Form.Group>
-							<Form.Label htmlFor='incorrect-answer-1'>
-								Incorrect Answer
-							</Form.Label>
-							<Form.Control type='text' id='incorrect-answer-1' required />
-						</Form.Group>
+						{/* Recall that incorrectAnswer is an array. */}
+						{question.incorrectAnswer.map((item, incorrectAnswerIndex) => {
+							return (
+								<Form.Group key={incorrectAnswerIndex}>
+									<Form.Label>Incorrect Answer</Form.Label>
+									<Form.Control
+										type='text'
+										placeholder='Enter an incorrect answer'
+										required
+									/>
+								</Form.Group>
+							);
+						})}
+						<Button
+							type='button'
+							onClick={() => handleIncorrectAnswerAdd(questionIndex)}>
+							+
+						</Button>
 					</div>
 				))}
 				<Button variant='primary' type='submit' className='questions-button'>
@@ -49,4 +89,5 @@ function NewQuizQuestions() {
 		</div>
 	);
 }
+
 export default NewQuizQuestions;
