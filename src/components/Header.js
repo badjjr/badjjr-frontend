9ffa3fa47c0d0e2eee049b8b4c92a7612f9657be
Jalynn <jalynnking@gmail.com>
2022-07-10 23:29:02 -0400

    Style header and displayQuiz components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import '/Users/jalynnjk/Desktop/sei/projects/unit-3/badjjr-frontend/src/styles/headerStyle.css';
 
 function Header(props) {
     return (
         <h1 className='badger-logo'>
-            <Link to='/'>🦡</Link>
+            <Link className='badger-home-link' to='/'>🦡</Link>
         </h1>
     );
 }
