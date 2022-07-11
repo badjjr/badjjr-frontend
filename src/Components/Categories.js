7c9fc1d1c405c11e4ed8b5fe67b1fd47888d462b
brianchoi93 <brianchoi93@gmail.com>
2022-07-10 22:30:29 -0700

    Make Home and Categories components

---
+++ src/Components/Categories.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Categories() {
+  return(
+    <div>
+      <ul>
+        <li>ANIMALS</li>
+        <li>GEOGRAPHY</li>
+        <li>SEIR-59</li>
+        <li>CELEBRITIES</li>
+        <li>HOUSEHOLD ITEMS</li>
+        <li>DEBUGGING</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Categories;
