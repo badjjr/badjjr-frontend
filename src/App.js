ef5930c6f080a47b395a72d94dff9a9a88410147
brianchoi93 <brianchoi93@gmail.com>
2022-07-10 22:32:58 -0700

    Set up routes for home and categories components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Home from './Components/Home';
+import Categories from './Components/Categories';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>
+        <Link to='/'><img src="public/badger.png" alt="Badger Icon" /></Link>
+      </h1>
+      <main>
+        <Routes>
+          <Route path = '/' element = { <Home /> } />
+          <Route path = '/categories' element = { <Categories /> } />
+        </Routes>
+      </main>
+    </>
+  )
 }
 
 export default App;
