a193441575c3a7d2e9a0b6c194fd558295b99284
jyaymie <jamiesujiepark@gmail.com>
2022-07-10 22:55:56 -0700

    Use Context for quiz data / Dynamically add input fields for additional incorrect answers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import React, { useState } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
 import { DataContext } from './dataContext';
+import { Routes, Route, Link } from 'react-router-dom';
 import NewQuizForm from './components/NewQuizForm/NewQuizForm';
 import NewQuizQuestions from './components/NewQuizQuestions/NewQuizQuestions';
 
 function App() {
-	const [numOfQuestions, setNumOfQuestions] = useState('');
+	// Use Context to share the following states between components.
+	const [quizFormData, setQuizFormData] = useState({
+		title: '',
+		numOfQuestions: 0,
+		category: '',
+	});
+
+	const [quizQuestions, setQuizQuestions] = useState([]);
 
 	return (
-		<DataContext.Provider value={{ numOfQuestions, setNumOfQuestions }}>
+		<DataContext.Provider
+			value={{
+				quizFormData,
+				setQuizFormData,
+				quizQuestions,
+				setQuizQuestions,
+			}}>
 			<div>
 				<header>
 					<h1>
--- src/components/NewQuizForm/NewQuizForm.js
+++ src/components/NewQuizForm/NewQuizForm.js
@@ -6,37 +6,50 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 function NewQuizForm() {
-	const { numOfQuestions, setNumOfQuestions } = useContext(DataContext);
+	const { setQuizFormData, setQuizQuestions } = useContext(DataContext);
+	const navigate = useNavigate();
 
-	// Programmatically navigate the user to '/new-quiz-questions' after form submission
-	let navigate = useNavigate();
-
-	const handleFormSubmit = (e) => {
+	const handleQuizFormSubmit = (e) => {
 		e.preventDefault();
-		setNumOfQuestions(numOfQuestions);
-		console.log(numOfQuestions);
+		// Store user input for '# of Questions' in a variable for easy referral.
+		const numOfQuestions = e.currentTarget['num-of-questions'].value;
+		// Sync form data to React context so that all components can access the
+		// updated information.
+		setQuizFormData({
+			title: e.currentTarget['title'].value,
+			numOfQuestions,
+			category: e.currentTarget['category'].value,
+		});
+		// Initialize a list of quiz questions, the length of which is based on the
+		// numOfQuestions value.
+		setQuizQuestions(
+			new Array(parseInt(numOfQuestions)).fill({
+				type: '',
+				question: '',
+				correctAnswer: '',
+				incorrectAnswer: [''],
+			})
+		);
+		// Navigate to the page holding the NewQuizQuestions component.
 		navigate('/new-quiz-questions');
 	};
 
 	return (
 		<div>
-			<Form className='quiz-form' onSubmit={handleFormSubmit}>
+			{/* Render the form with Bootstrap styling. */}
+			<Form className='quiz-form' onSubmit={handleQuizFormSubmit}>
 				<Form.Group>
 					<Form.Label htmlFor='title'>Title</Form.Label>
 					<Form.Control type='text' id='title' placeholder='French' required />
 				</Form.Group>
 				<Form.Group>
-					<Form.Label htmlFor='number-of-questions'># of Questions</Form.Label>
+					<Form.Label htmlFor='num-of-questions'># of Questions</Form.Label>
 					<Form.Control
 						type='number'
-						id='number-of-questions'
+						id='num-of-questions'
 						placeholder='10'
 						min='1'
 						max='20'
-						value={numOfQuestions}
-						onChange={(e) => {
-							setNumOfQuestions(e.target.value);
-						}}
 						required
 					/>
 				</Form.Group>
