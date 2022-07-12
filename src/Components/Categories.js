8a9db9750df5ad2bc26be2bc9cdfa72374fc9cf9
brianchoi93 <brianchoi93@gmail.com>
2022-07-12 16:28:43 -0700

    Display quizzes

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,17 +1,31 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-function Categories() {
-  return(
-    <div>
-      <ul>
-        <li>ANIMALS</li>
-        <li>GEOGRAPHY</li>
-        <li>SEIR-59</li>
-        <li>CELEBRITIES</li>
-        <li>HOUSEHOLD ITEMS</li>
-        <li>DEBUGGING</li>
-      </ul>
-    </div>
+function Categories(props) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://badjjr.herokuapp.com/api/quizzes")
+      .then((res) => res.json())
+      .then((json) => {
+        setCategories(json)
+      })
+      .catch(console.error);
+  }, []);
+
+  return (
+    <section>
+      {
+        categories.map((cats) => (
+          <div key={cats._id}>
+            <h3> Title: { cats.title } </h3>
+            <h4> No. of Questions: { cats.numberOfQuestions } </h4>
+            <h4> Category: { cats.category } </h4>
+          </div>
+        ))
+      }
+    </section>
   )
 }
 
