71473e79e6a8a58b4721e43456fe8e5b029c2b85
Jalynn <jalynnking@gmail.com>
2022-07-12 21:13:58 -0400

    Add score component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import DisplayQuiz from './components/DisplayQuiz'
 import Header from './components/Header'
+import Score from './components/Score'
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path='/quiz' element={<DisplayQuiz />} />
+        <Route path='/score' element={<Score />} />
       </Routes>
     </div>
   );
