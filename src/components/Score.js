d67f73769faa9321356cec2ffbb9be18bf545773
Jalynn <jalynnking@gmail.com>
2022-07-12 23:49:40 -0400

    Write scoring algorithm using hardcoded user answers

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,10 +1,60 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function Score({quizAnswers}) {
-    console.log(quizAnswers)
+function Score(props) {
+    const [userAnswers, setUserAnswers] = useState()
+    const [correctAnswers, setCorrectAnswers] = useState()
+    const [userCorrectAnswers, setUserCorrectAnswers] = useState()
+    const [score, setScore] = useState()
+
+    let uanswers = { 0: "JavaScript", 1: "Mocha", 2: "False", 3: "git checkout dev", 4: "True, True", 5: "Amazon", 6: "False", 7: "Props", 8: "Response Evaluation Process Line used in asynchronous error handling with the Try/Catch syntax", 9: "True"};
+
+    let correctAnswersTemp = []
+
+    async function getCorrectAnswers() {
+        const response = await axios.get(
+					`https://badjjr.herokuapp.com/api/quizzes/62cc994fc582b1dd9b70a621`
+				)
+        response.data.questions.forEach((question) => {
+            correctAnswersTemp.push(question.correctAnswer)
+        })
+        setCorrectAnswers(correctAnswersTemp)
+    }
+
+    function compareAnswers() {
+        let sameAnswer = userAnswers.filter((answer) => {
+           if  (correctAnswers.includes(answer)) {
+            return true
+           } else return false
+        })
+        setUserCorrectAnswers(sameAnswer);
+    }
+
+    function gradeQuiz() {
+        let pointsPerQuestion = 100/correctAnswers.length
+        let scoreTemp = userCorrectAnswers.length * pointsPerQuestion
+        setScore(scoreTemp)
+    }
+    useEffect(() => {
+        setUserAnswers(Object.values(uanswers));
+        getCorrectAnswers()
+    }, [])
+
+    useEffect(() => {
+        if (correctAnswers) {
+            compareAnswers()
+        }
+    }, [correctAnswers])
+
+    useEffect(() => {
+        if (userCorrectAnswers) {
+            gradeQuiz()
+        }
+    }, [userCorrectAnswers])
     return (
         <div>
-            score
+            Your result is: {score}%
         </div>
     );
 }
