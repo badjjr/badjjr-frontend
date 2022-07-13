b2ed5d321035a8ac3ad8e152fc1d31258ed229b1
Jalynn <jalynnking@gmail.com>
2022-07-13 15:11:38 -0400

    Use context hook to share data between components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { DataContext } from './dataContext';
 import { Routes, Route } from 'react-router-dom';
-import DisplayQuiz from './components/DisplayQuiz'
-import Header from './components/Header'
-import Score from './components/Score'
+import DisplayQuiz from './components/DisplayQuiz';
+import Header from './components/Header';
+import Score from './components/Score';
+
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path='/quiz' element={<DisplayQuiz />} />
-        <Route path='/score' element={<Score />} />
-      </Routes>
-    </div>
-  );
+	const [quizAnswers, setQuizAnswers] = useState([]);
+
+	return (
+		<DataContext.Provider
+			value={{
+				quizAnswers,
+        setQuizAnswers
+			}}>
+			<div className='App'>
+				<Header />
+				<Routes>
+					<Route path='/quiz/:id' element={<DisplayQuiz />} />
+					<Route path='/score' element={<Score />} />
+				</Routes>
+			</div>
+		</DataContext.Provider>
+	);
 }
 
 export default App;
